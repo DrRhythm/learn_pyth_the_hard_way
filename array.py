4c5ad177b7f84770425f5579cc245aa2d6421776
Wayne D. Paden <wpaden@godaddy.com>
2018-05-26 13:25:09 +0200

    syncing repo up

--- array.py
+++ array.py
@@ -1,3 +1,12 @@
-servers = "{} {} {} {}"
+# Given a list (vector), write a function "partial_reverse(a, i, j)"
+# that will reverse the items in the array a starting at i and ending at j.
+# For example, given the array equal to (A, B, C, D, E, F, G):
+# a = (A, B, C, D, E, F, G)
+# partial_reverse(a, 2, 5)
+# result: (A, B, F, E, D, C, G)
 
-print(servers.format("A", "B", "C", "D")
+# base list
+pylist = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+
+print("List now : ", pylist[0])
+#partial_reverse = []
